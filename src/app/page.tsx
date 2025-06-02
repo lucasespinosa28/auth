b87467775acd9f13a0af4b6b1cd946a3e605b1c9
lucas espinosa <email@lucasespinosa.com>
2025-06-02 14:07:13 -0300

    new urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { useSession } from 'next-auth/react';
 import { useAccount } from 'wagmi';
 
 export default function Home() {
+  const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
+const VERCEL_URL = process.env.VERCEL_URL;
   const { data: session, status } = useSession();
   const { address, isConnected } = useAccount();
 
@@ -15,7 +17,8 @@ export default function Home() {
           <ConnectButton />
         </div>
       </div>
-
+      <h2>NEXTAUTH_URL:{NEXTAUTH_URL}</h2>
+       <h2>VERCEL_URL:{VERCEL_URL}</h2>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-8">
